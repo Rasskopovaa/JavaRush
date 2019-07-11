4ba3544ea1f5a68dbb119ec09548beb7560725e5
Rasskopovaa <raskopovaa@gmail.com>
2019-07-12 00:29:10 +0700

    Уровень 3

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0305/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task03.task0305;
+
+/* 
+Я так давно родился
+*/
+
+
+
+public class Solution {
+    public static void main(String[] args) {
+        printString("MARCH 10 2002");
+
+    }
+    public static void printString(String s){
+        System.out.println(s);
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task03.task0308;
+
+/* 
+Произведение 10 чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<10){
+           i*=i;
+        }
+        System.out.println(i);
+
+    }
+}
