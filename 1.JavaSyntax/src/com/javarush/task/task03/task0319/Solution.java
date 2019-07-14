ff341c2f265a0b59a9d091f4cbeda940ca66d064
Rasskopovaa <raskopovaa@gmail.com>
2019-07-14 21:28:15 +0700

    Уровень 3

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task03.task0319;
+
+/* 
+Предсказание на будущее
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+
+        Scanner sc = new Scanner(System.in);
+         String name=sc.nextLine();
+        int numberA = sc.nextInt();
+        int numberB=sc.nextInt();
+        System.out.println(name +" "+"получает"+" "+ numberA+" "+"через"+" "+ numberB+" "+"лет");
+            }
+        }
