0f4e66020085c1a88aba7ece35941fd5b43ec6ad
Rasskopovaa <raskopovaa@gmail.com>
2019-08-05 19:29:04 +0700

    Уровень 7

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task07.task0706;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Улицы и дома
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a[] = new int[15];
+        int b = 0;
+        int c = 0;
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Integer.parseInt(reader.readLine());
+
+            if (i % 2 == 0)
+                b += a[i];
+            else
+                c += a[i];
+        }
+        if (b > c)
+            System.out.println("В домах с четными номерами проживает больше жителей.");
+        else
+            System.out.println("В домах с нечетными номерами проживает больше жителей.");
+    }
+}
