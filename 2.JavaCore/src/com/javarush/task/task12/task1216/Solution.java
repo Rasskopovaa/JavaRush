296e4b5142d08d6b8665869cbd647821a6f16c50
Rasskopovaa <raskopovaa@gmail.com>
2019-08-21 16:21:12 +0700

    Уровень 12

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1216/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1216;
+
+/* 
+Летать охота
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void Transport(String s);
+
+        public int fly(int a);
+    }
+    //add an interface here - добавь интерфейс тут
+
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public int fly(int a);
+    }
+
+    public interface CanRun {
+        public void run(String d);
+    }
+
+    public interface CanSwim {
+        public int swim(String s);
+    }
+
+//add interfaces here - добавь интерфейсы тут
+
+}
